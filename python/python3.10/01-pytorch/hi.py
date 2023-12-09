dbbfaa56a6537a8592e56d05bf91db58043f1133
Ian Eyberg <eyberg@Ians-MacBook-Pro.local>
2023-12-09 07:51:41 -0800

    .

--- python/python3.10/01-pytorch/hi.py
+++ python/python3.10/01-pytorch/hi.py
@@ -6,16 +6,12 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.hidden_layer = nn.Linear(1, 1)
-        self.hidden_layer.weight =
-torch.nn.Parameter(torch.tensor([[1.58]]))
-        self.hidden_layer.bias =
-torch.nn.Parameter(torch.tensor([-0.14]))
+        self.hidden_layer.weight = torch.nn.Parameter(torch.tensor([[1.58]]))
+        self.hidden_layer.bias = torch.nn.Parameter(torch.tensor([-0.14]))
 
         self.output_layer = nn.Linear(1, 1)
-        self.output_layer.weight =
-torch.nn.Parameter(torch.tensor([[2.45]]))
-        self.output_layer.bias =
-torch.nn.Parameter(torch.tensor([-0.11]))
+        self.output_layer.weight = torch.nn.Parameter(torch.tensor([[2.45]]))
+        self.output_layer.bias = torch.nn.Parameter(torch.tensor([-0.11]))
 
     def forward(self, x):
         x = torch.sigmoid(self.hidden_layer(x))
