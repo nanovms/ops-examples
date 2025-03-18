29eb65393f291d7d863c43cd33cbfb9fd111c818
Ian Eyberg <ian@nanovms.com>
2025-03-17 21:29:55 -0700

    adding in racket example

---
+++ racket/README.md
@@ -0,0 +1,14 @@
+Racket Hello World Unikernel
+==================
+
+Compile:
+
+```sh
+raco exe --orig-exe web.rkt
+```
+
+Run:
+
+```sh
+ops run web -p 8080
+```
---
+++ racket/web.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (serve port-no)
+  (define listener (tcp-listen port-no 5 #t))
+  (define (loop)
+    (accept-and-handle listener)
+    (loop))
+  (loop))
+
+(define (accept-and-handle listener)
+  (define-values (in out) (tcp-accept listener))
+  (handle in out)
+  (close-input-port in)
+  (close-output-port out))
+
+(define (handle in out)
+  (regexp-match #rx"(\r\n|^)\r\n" in)
+  (display "HTTP/1.0 200 Okay\r\n" out)
+  (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
+  (display "<html><body>Hello, world!</body></html>" out))
+(serve 8080)
